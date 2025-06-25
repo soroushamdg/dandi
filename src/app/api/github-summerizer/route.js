825e0911e17f30750b3549339b1c0145fa70f123
soroushamdg <soroushmpour@gmail.com>
2025-06-25 15:39:19 -0400

    added error catch

--- src/app/api/github-summerizer/route.js
+++ src/app/api/github-summerizer/route.js
@@ -29,6 +29,7 @@ export async function POST(request) {
     console.log(output);
     return Response.json({ summary: output.summery, cool_facts: output.cool_facts });
   } catch (error) {
+    console.error(error);
     return Response.json({ valid: false, message: 'Server error.' }, { status: 500 });
   }
 } 
