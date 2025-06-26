3cbb9eaba00b76cab371eb45954375dfa8d5d28e
soroushamdg <soroushmpour@gmail.com>
2025-06-26 12:10:12 -0400

    updated default value for strings

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -5,8 +5,8 @@ import { supabase } from "../../../../lib/supabase";
 export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
   ],
   session: {
